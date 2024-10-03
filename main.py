4027bdb280c057bc9bad1c407a477e31037f2bbf
Luedvi <luedvi@outlook.com>
2024-10-02 18:16:32 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-print('''              ,.  _~-.,               .
-           ~'`_ \/,_. \_
-          / ,"_>@`,__`~.)             |           .
-          | |  @@@@'  ",! .           .          '
-          |/   ^^@     .!  \          |         /
-          `' .^^^     ,'    '         |        .             .
-           .^^^   .          \                /          .
-          .^^^       '  .     \       |      /       . '
-.,.,.     ^^^             ` .   .,+~'`^`'~+,.     , '
-&&&&&&,  ,^^^^.  . ._ ..__ _  .'             '. '_ __ ____ __ _ .. .  .
-%%%%%%%%%^^^^^^%%&&;_,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,
-&&&&&%%%%%%%%%%%%%%%%%%&&;,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=
-%%%%%&&&&&&&&&&&%%%%&&&_,.;^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,
-%%%%%%%%%&&&&&&&&&-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-==--^'~=-.,__,.-=~'
-##mjy#####*"'
-_,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,.-=~'`^`'~=-.,__,.-=~'
-
-~`'^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^`'~=-.,__,.-=~'`^''')
-print("Bienvenida a la aventura.")
-print("Elige tu destino") 
-
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
-crossroad1=input("Te ganas un viaje para bucear en Hawai o apostar en las Vegas.\n¿A dónde vas? Escribe \"hawai\" o \"vegas\"\n").lower()
-if crossroad1=="hawai":#or crossroad1=="Hawai" or crossroad1=="HAWAI"
-  crossroad2=input("Llegas a Hawai y tomas un paseo en bote para ir a bucear.\nAl emerger te ofrecen un viaje en avión privado a Japón.\nEscribe \"volar\" para ir a japón o \"esperar\" para quedarte a\ndisfrutar un poco mas de la playa\n").lower()
-  if crossroad2=="esperar":
-    crossroad3=input("El guia de turistas te da 3 alternativas para pasar el resto de la tarde.\nIr a la selva a explorar, ir a surfear o ir al centro comercial.\nEscribe \"selva\", \"surf\" o \"centro\"\n").lower()
-    if crossroad3=="selva":
-      print("Has sido devorada por una tribu de canibales.\nFin del juego")
-    elif crossroad3=="surf":
-      print("Has sido devorada por un tiburón.\nFin del juego")
-    elif crossroad3=="centro":
-      print("Compras algunos souvenirs y regresas a casa sana y salva.\n¡Felicidades has ganado el juego!")
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
     else:
-      print("Esa no es una opción válida, te cae un meteorito y mueres al instante.\nFin del juego")
+      print("You chose a door that doesn't exist. Game Over.")
   else:
-    print("A 3,600m todos sufren de enfermedad por descompresión y se estrellan,\nlo que hace que el avión explote y mate a todos a bordo.\nFin del juego")
+    print("You get attacked by an angry trout. Game Over.")
 else:
-  print("Apostaste todo tu dinero en el casino y ahora estas en bancarrota.\nFin del juego")
-  
+  print("You fell into a hole. Game Over.")
